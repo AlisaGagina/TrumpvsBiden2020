e9ec9d72f683ac4b418a809a8a4c16c7552cd52b
Sarah Hafez <sarah@ip-172-31-58-37.ec2.internal>
2020-11-24 18:04:08 +0000

    outputs a tsv&csv file containing the name and title of post

---
+++ scripts/filter.py
@@ -0,0 +1,45 @@
+import os.path as osp
+import argparse
+import json
+import pandas as pd
+
+def main():
+    script_dir = osp.dirname(__file__)
+    names = []
+    titles = []
+
+
+    # Extracts Json lines from the files in data and adds it to the names and title list
+    for i in range(1,4):
+        with open(osp.join(script_dir,'..','data',f'reddit2020112{i}.json')) as fin:
+            lines = fin.readlines()
+            names.extend([json.loads(lines[x])['data']['name'] for x in range(len(lines))])
+            titles.extend([json.loads(lines[x])['data']['title'] for x in range(len(lines))])
+
+    #print(names, titles)
+
+    index_to_keep = [] #has the index of all lines we wanna keep
+
+    #populates the index_to_keep
+    for i in range(len(titles)):
+        if ("trump" or "biden" or "donald" or "joe") in titles[i].lower():
+            index_to_keep.append(i)
+
+    #keeps data we wanna keep
+    names = [names[i] for i in index_to_keep]
+    titles = [titles[i] for i in index_to_keep]
+
+    df = pd.DataFrame({'name': names, 'title': titles})
+    df["coding"] = ""
+
+    output_file_tsv = osp.join(script_dir,'..','data','filtered_data.tsv')
+    df.to_csv(output_file_tsv, sep='\t', index=False)
+
+    output_file_csv = osp.join(script_dir, '..', 'data', 'filtered_data.csv')
+    df.to_csv(output_file_csv, index=False)
+
+    #print(len(names),len(titles))
+    #print(names, titles)
+
+if __name__ =='__main__':
+    main()
