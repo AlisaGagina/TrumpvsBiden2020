8b2a10514caed9682cc91439112746ed9d2865c2
Noam cohen <cohen.noam@hotmail.com>
2020-11-25 13:42:52 -0500

    wrote code to compute tfidf

---
+++ scripts/tfidf.py
@@ -0,0 +1,46 @@
+import argparse
+import pandas as pd
+import os
+import csv
+import math
+
+def tfidf(df):
+	total = {}
+	subCount={}
+	for index,row in df.iterrows():
+		sub = row['subreddit']
+		coding = row['coding']	
+		if (sub not in subCount):
+			subCount[sub]={}
+			subCount[sub][coding] = 1
+		elif (coding not in subCount[sub]):
+			subCount[sub][coding] = 1
+		else:
+			subCount[sub][coding] += 1
+		if (coding not in total):
+			total[coding] = 1
+		else:
+			total[coding] += 1
+	
+	tfidf={}
+	for (key,value) in subCount.items():
+		tfidf[key]=value
+		for (code,coding) in value.items():
+			tfidf[key][code]=math.log10(total[code]/coding)
+	for (key,value) in subCount.items():
+		print(key)
+		for (code,coding) in value.items():
+			print("\t"+str(code)+", "+str(coding))
+
+def main():
+	total={}
+	subCount={}
+	parser = argparse.ArgumentParser()
+	parser.add_argument("input",help="files containing the posts")
+
+	args = parser.parse_args()
+	df = pd.read_csv(args.input,sep="\t")	
+	tfidf(df)
+
+if __name__ == '__main__':
+	main()
